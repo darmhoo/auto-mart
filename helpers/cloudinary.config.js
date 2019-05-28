302c00bd55886ca9ba6d57f79d77ec70fe0f8493
Omodamola <darmhoo@hotmail.com>
2019-05-28 13:19:31 +0100

    added coveralls

---
+++ helpers/cloudinary.config.js
@@ -0,0 +1,12 @@
+const cloudinary = require('cloudinary').v2;
+
+
+cloudinary.config({
+  cloud_name: 'ded69cslb',
+  api_key: '622468466571416',
+  api_secret: 'rfjbTi9AY5Zhc0XkILC_fYADbB8',
+});
+
+module.exports = {
+  cloudinary,
+};
